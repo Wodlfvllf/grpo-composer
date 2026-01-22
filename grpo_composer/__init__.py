93c49bb01a71f067595699f5a1aefa4f24ee9a5e
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2026-01-22 20:04:56 +0530

    correct init files

---
+++ grpo_composer/__init__.py
@@ -0,0 +1,10 @@
+
+from .rewards import RewardCalculator
+from .rewards import BinaryRewardCalculator
+from .rewards import DiversityAdjustedRewardCalculator
+
+__all__ = [
+    "RewardCalculator",
+    "BinaryRewardCalculator",
+    "DiversityAdjustedRewardCalculator",
+]
