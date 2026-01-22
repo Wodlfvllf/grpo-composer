bf628d1e46077f84d6e39fe4f1feba6f0dec43cc
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2026-01-22 18:09:41 +0530

    Binary Reward Calculator

--- grpo_composer/rewards/binary.py
+++ grpo_composer/rewards/binary.py
@@ -1,4 +1,8 @@
-
+"""
+Binary Reward Calculator
+This class Implements Binary Reward Calculator for Base GRPO class to 
+calculate binary rewards which would then be used in Advantage Calculation.
+"""
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
@@ -6,15 +10,15 @@ from torch.nn import functional as F
 from .base import RewardCalculator
 
 class BinaryRewardCalculator(RewardCalculator):
-    def __init__(self, rewards: list, threshold: float, **kwargs):
+    def __init__(self, rewards: list, threshold: bool, **kwargs):
         super().__init__(rewards, **kwargs) 
         self.rewards = rewards
         self.threshold = threshold
 
     def calculate(self):
-        if self.threshold is None:
+        if self.threshold:
             return [torch.where(reward > 0.5, torch.ones_like(reward), torch.zeros_like(reward)) for reward in self.rewards]
         else:
             return self.rewards
 
-            
+
