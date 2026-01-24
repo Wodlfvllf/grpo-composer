6d87389b8f45847e4afcdddfdce78653a8565345
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2026-01-24 20:59:57 +0530

    adding more reward functions

--- grpo_composer/__init__.py
+++ grpo_composer/__init__.py
@@ -1,10 +1,30 @@
+"""
+GRPO Composer - Main Package
 
-from .rewards import RewardCalculator
-from .rewards import BinaryRewardCalculator
-from .rewards import DiversityAdjustedRewardCalculator
+Modular GRPO framework for LLM reinforcement learning.
+"""
+
+from .rewards import (
+    RewardCalculator,
+    BinaryRewardCalculator,
+    DiversityAdjustedRewardCalculator,
+    FrequencyAwareRewardCalculator,
+    LengthDependentRewardCalculator,
+    PosteriorCompositeRewardCalculator,
+    MultiRewardProcessor,
+    RewardConfig,
+)
 
 __all__ = [
+    # Reward Calculators
     "RewardCalculator",
     "BinaryRewardCalculator",
     "DiversityAdjustedRewardCalculator",
+    "FrequencyAwareRewardCalculator",
+    "LengthDependentRewardCalculator",
+    "PosteriorCompositeRewardCalculator",
+    "MultiRewardProcessor",
+    "RewardConfig",
 ]
+
+__version__ = "0.1.0"
