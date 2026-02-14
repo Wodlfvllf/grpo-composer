60176af20232e1f16db9ff62879e3d94d29cfa3f
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2026-02-14 14:30:44 +0530

    Abstract base class for configuration management.

--- grpo_composer/interfaces/config.py
+++ grpo_composer/interfaces/config.py
@@ -32,3 +32,24 @@ Implemented by:
 
 Used throughout codebase for type-safe configuration.
 """
+
+from abc import ABC, abstractmethod, classmethod
+import torch
+import torch.nn as nn
+
+class Config(ABC):
+
+    @abstractmethod
+    def validate(self):
+        '''Validate config parameters.'''
+        pass
+
+    @abstractmethod
+    def to_dict(self):
+        '''Serialize config to dict.'''
+        pass
+
+    @classmethod
+    def from_yaml(cls, ppath:str) -> 'Config':
+        '''Load config from YAML file.'''
+        pass
