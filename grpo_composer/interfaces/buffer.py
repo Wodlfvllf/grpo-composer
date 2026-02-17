53f99136bdb17e92e2bfa774e0d8a6d6ee0b5c91
Shashank Shekhar Shukla <shuklashashankshekhar863@gmail.com>
2026-02-17 16:21:49 +0530

    MOdified functions in abstract buffer class

--- grpo_composer/interfaces/buffer.py
+++ grpo_composer/interfaces/buffer.py
@@ -30,7 +30,7 @@ Implemented by:
 - `rollouts/buffer/redis.py` - Distributed Redis
 - `rollouts/buffer/disk.py` - Persistent storage
 """
-
+import torch
 from torch.utils.data import Dataset
 from typing import List
 from abc import ABC, abstractmethod
@@ -38,21 +38,28 @@ from dataclasses import dataclass, field
 
 @dataclass
 class BufferEntry:
-    buffer: list = field(default_factory=list)
+    prompt_tokens     : torch.Tensor #(T_prompt,)
+    completions       : torch.Tensor #(G, T_completion)
+    policy_log_probs  : torch.Tensor #(G, T_completion)
+    ref_log_probs     : torch.Tensor #(G, T_completion)
+    rewards           : torch.Tensor #(G,)
+    completion_lengths: torch.Tensor #(G,)
+    mean_accuracy     : float   #(μ_q)
+    metadata          : dict
 
 
 class Buffer(ABC):
-
     @abstractmethod
-    def insert(self, entries : List[BufferEntry]) -> None:
-        pass
-
+    def insert(self, entries: List[BufferEntry]) -> None:
+        '''Add new rollout entries to the buffer.'''
     @abstractmethod
-    def get_by_prompt_id(self, prompt_id : str) -> List[BufferEntry]:
-        pass
-
+    def get_all(self) -> List[BufferEntry]:
+        '''Return all entries (no filtering).'''
     @abstractmethod
-    def get_unique_prompt_ids(self) -> List[str]:
-        pass
+    def clear(self) -> None:
+        '''Remove all entries. Called after each on-policy training step.'''
+    @abstractmethod
+    def current_size(self) -> int:
+        '''Number of entries currently stored.'''
 
 
